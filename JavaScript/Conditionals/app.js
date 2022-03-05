39f545159e4f63a6a8a1f16c99af37bbd56a5ba4
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-03-04 19:11:24 -0500

    first commit

---
+++ JavaScript/Conditionals/app.js
@@ -0,0 +1,33 @@
+//Using if and else
+// let ran = Math.random();
+// if (ran < 0.5) {
+//   console.log("Your # is less than 0.5");
+// } else {
+//   console.log("Your # is greater than 0.5");
+// }
+
+//Using if else-if and else
+// const dayOfWeek = prompt("Enter a day:");
+
+// if (dayOfWeek == "monday") {
+//   console.log("It is a Monday....");
+// } else if (dayOfWeek == "saturday") {
+//   console.log("Time to game!");
+// } else if (dayOfWeek == "sunday") {
+//   console.log(":( I work tomorrow....");
+// } else {
+//   console.log("This day sucks! or does it..");
+// }
+
+//Using if else-if and else
+const age = 68;
+
+if (age < 5) {
+  console.log("you are a child so free entry");
+} else if (age < 10) {
+  console.log("you have to pay 10$");
+} else if (age < 65) {
+  console.log("20$ is the feed");
+} else {
+  console.log("You a senior, You get a discount! Pay $10");
+}
---
+++ JavaScript/Running_Js_Script/app.js
@@ -0,0 +1,3 @@
+console.log("Hello from out first JS  File");
+let total = 1 + 3;
+console.log(total);
