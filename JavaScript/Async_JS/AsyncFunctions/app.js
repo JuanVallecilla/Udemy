6e52a0f8376fee8d37011460101592e8d27556c9
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-03-27 22:07:10 -0400

    Finished Async Section

--- JavaScript/Async_JS/AsyncFunctions/app.js
+++ JavaScript/Async_JS/AsyncFunctions/app.js
@@ -1,107 +1,88 @@
-// async function hello() {
+// Declaring a function as async automatically returns a promise.
+async function hello() {}
+
+// async declaration with arrow functions
+// const sing = async () => {
+// Rejecting a Promise by throwing an error (Eg. using throw)
+//   throw new Error("Oppsi");
+//   return "Do Re Mi";
+// };
+
+// sing()
+//   .then((data) => {
+//     console.log("Promise resolve with:", data);
+//   })
+//   .catch((err) => {
+//     console.log("Promise Rejected");
+//     console.log(err);
+//   });
+
+// Making a "dumb real-world" Example
+const login = async (username, password) => {
+  if (!username || !password) throw "Missing Credentials";
+  if (password === "password") return "welcome";
+  throw "Invalid Password";
+};
+
+login("MadzGod", "password")
+  .then((msg) => {
+    console.log("You are log In!");
+    console.log(msg);
+  })
+  .catch((err) => {
+    console.log("Error");
+    console.log(err);
+  });
+
+// Using async with await
+// const colorChanger = (newColor, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.background = newColor;
+//       resolve();
+//     }, delay);
+//   });
+// };
+
+// async function rainbow() {
+//   await colorChanger("red", 1000);
+//   await colorChanger("orange", 1000);
+//   await colorChanger("yellow", 1000);
+//   await colorChanger("blue", 1000);
+//   await colorChanger("indigo", 1000);
+//   await colorChanger("violet", 1000);
+//   return "Done";
+// }
+// // rainbow().then(() => console.log("Taste The Rainbow"));
+// async function printRainbow() {
+//   await rainbow();
+//   console.log("You reached the end of the rainbow");
 // }
 
-
-const sing = async () => {
-    throw "OH NO, PROBLEM!"
-    return 'LA LA LA LA'
-}
-
-sing()
-    .then(data => {
-        console.log("PROMISE RESOLVED WITH:", data)
-    })
-    .catch(err => {
-        console.log("OH NO, PROMISE REJECTED!")
-        console.log(err)
-    })
-
-
-
-
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if (password === 'corgifeetarecute') return 'WELCOME!'
-    throw 'Invalid Password'
-}
-
-login('todd', 'corgifeetarecute')
-    .then(msg => {
-        console.log("LOGGED IN!")
-        console.log(msg)
-    })
-    .catch(err => {
-        console.log("ERROR!")
-        console.log(err)
-    })
-
-
-
-
-
-
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay)
-    })
-}
-
-// delayedColorChange('red', 1000)
-//     .then(() => delayedColorChange('orange', 1000))
-//     .then(() => delayedColorChange('yellow', 1000))
-//     .then(() => delayedColorChange('green', 1000))
-//     .then(() => delayedColorChange('blue', 1000))
-//     .then(() => delayedColorChange('indigo', 1000))
-//     .then(() => delayedColorChange('violet', 1000))
-
-
-async function rainbow() {
-    await delayedColorChange('red', 1000)
-    await delayedColorChange('orange', 1000)
-    await delayedColorChange('yellow', 1000)
-    await delayedColorChange('green', 1000)
-    await delayedColorChange('blue', 1000)
-    await delayedColorChange('indigo', 1000)
-    await delayedColorChange('violet', 1000)
-    return "ALL DONE!"
-}
-
-// rainbow().then(() => console.log("END OF RAINBOW!"))
-
-
-async function printRainbow() {
-    await rainbow();
-    console.log("END OF RAINBOW!")
-}
-
-printRainbow();
+// printRainbow();
 
 const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 2000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
-}
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
 
-
-async function makeTwoRequests() {
-    try {
-        let data1 = await fakeRequest('/page1');
-        console.log(data1);
-        let data2 = await fakeRequest('/page2');
-        console.log(data2);
-    } catch (e) {
-        console.log("CAUGHT AN ERROR!")
-        console.log("error is:", e)
-    }
-
+// Also handling errors with Async Functions
+async function makeTwoResquests() {
+  try {
+    // we can store await into a variable
+    let data1 = await fakeRequest("/page1");
+    console.log(data1);
+    let data2 = await fakeRequest("/page2");
+    console.log(data2);
+  } catch (e) {
+    console.log("Error Caught:", e);
+  }
 }
