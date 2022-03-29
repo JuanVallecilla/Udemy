66fcd9e793bdd4b42b248120a5dcf09428c38eef
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-03-29 17:19:39 -0400

    First attemped at tv Show API search

---
+++ JavaScript/Ajax/TVShowSearch/app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchInput = form.elements.query.value;
+  const config = { params: { q: searchInput } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  //console.log(res.data[0].show.image.medium);
+  displayImg(res.data);
+  form.elements.query.value = "";
+});
+
+const displayImg = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
