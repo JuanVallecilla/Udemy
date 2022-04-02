b34a340a3183dbd9485cd7d8bcd1755c1b8282c9
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-02 18:55:28 -0400

    displays name and rating of the show.

--- JavaScript/Ajax/TVShowSearch/app.js
+++ JavaScript/Ajax/TVShowSearch/app.js
@@ -1,8 +1,5 @@
-// const form = document.getElementById("searchForm");
 const container = document.querySelector(".container");
 const input = document.querySelector("#search__input");
-// const allDiv = document.querySelector(".tvshowCard");
-// const div = document.querySelectorAll("div");
 
 input.addEventListener("input", async function (e) {
   e.preventDefault();
@@ -12,33 +9,40 @@ input.addEventListener("input", async function (e) {
   const searchInput = this.value;
   const config = { params: { q: searchInput } };
   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
-  displayImg(res.data);
-  console.log(res.data);
+  displayCard(res.data);
 });
 
-const displayImg = (shows) => {
+const displayCard = (shows) => {
   for (let result of shows) {
     if (result.show.image) {
       const tvCard = document.createElement("div");
       tvCard.classList.add("tvCard");
+      const tvshowName = document.createElement("figcaption"); // figcaption is better here?
+      if (result.show.rating.average) tvshowName.innerText = `${result.show.name} \n Rating: ${result.show.rating.average}`;
+      else tvshowName.innerText = `${result.show.name}\nRating: "N/A`;
+      // const rating = document.createElement("figcaption");
+      // if (result.show.rating.average) rating.innerHTML = `Rating: ${result.show.rating.average}`;
+      // else rating.innerHTML = `Rating: "N/A"`;
       const frontFace = document.createElement("div");
       frontFace.classList.add("frontFace");
       const backFace = document.createElement("div");
       backFace.classList.add("backFace");
+      backFace.innerHTML = result.show.summary;
       const showImg = document.createElement("img");
       showImg.src = result.show.image.medium;
-      const summary = document.createElement("p");
-      summary.innerHTML = result.show.summary;
       frontFace.appendChild(showImg);
+      frontFace.appendChild(tvshowName);
+      // frontFace.appendChild(rating);
       container.appendChild(frontFace);
       tvCard.appendChild(frontFace);
       tvCard.appendChild(backFace);
-      backFace.appendChild(summary);
       container.appendChild(tvCard);
     }
   }
 };
 
+// Function to delete tv show Images since when user is searching for tv shows it
+// needs to remove old images that were previously displayed
 const deleteImg = () => {
   const imgs = document.querySelectorAll("img");
   for (const img of imgs) {
@@ -46,6 +50,8 @@ const deleteImg = () => {
   }
 };
 
+// Function to delete tv show summary since when user is searching for tv shows it
+// needs to remove old summaries that were previously displayed
 const deleteSummary = () => {
   const summary = document.querySelectorAll("p");
   for (const p of summary) {
@@ -53,12 +59,14 @@ const deleteSummary = () => {
   }
 };
 
-const removeChild = () => {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
-};
-
 const removeEmptyDivs = () => {
   container.innerHTML = "";
 };
+
+//function to remove html tags from the summary summary info we get from the api on the tv show.
+// For debugging only.
+function stripHtml(html) {
+  let tmp = document.createElement("p");
+  tmp.innerHTML = html;
+  return tmp.textContent || tmp.innerText || "";
+}
