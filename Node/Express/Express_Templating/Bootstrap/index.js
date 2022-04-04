6a4178e45fdc16be8c3e439f3b903db220195d03
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-04 17:38:08 -0400

    Templating with Bootstrap

---
+++ Node/Express/Express_Templating/Bootstrap/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const redditData = require("./data.json");
+
+// Serving static files in Express (in subreddit template)
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { num: num });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const data = redditData[subreddit];
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("notFound", { subreddit });
+  }
+});
+
+app.get("/dogs", (req, res) => {
+  const dogs = ["Coco", "Astro", "Scooby"];
+  res.render("dogs", { dogs });
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 3000");
+});
