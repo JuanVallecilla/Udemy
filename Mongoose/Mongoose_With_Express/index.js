d5595de4e4551451d6f574afd95b4ea6bfd574e7
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-09 19:58:07 -0400

    Updating Products

--- Mongoose/Mongoose_With_Express/index.js
+++ Mongoose/Mongoose_With_Express/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 // https://mongoosejs.com/docs/guide.html
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const methodOverride = require("method-override");
 
 mongoose
   .connect("mongodb://localhost:27017/farmStand", { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,7 +18,9 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products/new", (req, res) => {
   res.render("products/new");
@@ -41,6 +44,18 @@ app.get("/products/:id", async (req, res) => {
   //   console.log(product);
   //   console.log("details page");
   res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const update = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+  res.redirect(`/products/${update._id}`);
 });
 
 app.listen(8080, () => {
