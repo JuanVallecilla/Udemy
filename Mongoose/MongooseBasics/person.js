6518faeb8905ad335e31f66c6423abee2a6dee9a
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-08 20:19:57 -0400

    Defining Mongoose Middleware

--- Mongoose/MongooseBasics/person.js
+++ Mongoose/MongooseBasics/person.js
@@ -18,4 +18,15 @@ personSchema.virtual("fullName").get(function () {
   return `${this.first} ${this.last}`;
 });
 
+// Pre middleware functions are executed one after another, when each middleware calls next.
+personSchema.pre("save", async function () {
+  this.first = "YO";
+  this.last = "MAMA";
+  console.log("About to save");
+});
+// Post middleware are executed after the hooked method and all of its pre middleware have completed.
+personSchema.post("save", async function () {
+  console.log("Just saved");
+});
+
 const Person = mongoose.model("Person", personSchema);
