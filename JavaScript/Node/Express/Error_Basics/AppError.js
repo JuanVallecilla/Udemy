4420910ce362f26d29a86c1c0da9d7dd3a67c5f6
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-14 19:09:36 -0400

    Handling Errors In Express Apps

---
+++ JavaScript/Node/Express/Error_Basics/AppError.js
@@ -0,0 +1,10 @@
+// Custom Error Class
+class AppError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
+
+module.exports = AppError;
